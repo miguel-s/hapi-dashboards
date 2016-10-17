a6b4a112070240fa2f8a44bfcaba6108ae08d7c1
Miguel Serrano <mlsv89@gmail.com>
2016-10-17 20:01:20 +0200

    select specific fields

--- controllers/socialnetworks_venue.js
+++ controllers/socialnetworks_venue.js
@@ -4,7 +4,7 @@ const Boom = require('boom');
 
 module.exports = function handler(request, reply) {
   const sales = new Promise((resolve, reject) => {
-    request.server.app.db.get('SELECT * FROM sn_venues_sales WHERE cd_pdv = ? ORDER BY 3 DESC', request.params.id,
+    request.server.app.db.all('SELECT reference, sum_one_month_val_hl, sum_three_months_val_hl FROM sn_venues_sales WHERE cd_pdv = ? ORDER BY 3 DESC', request.params.id,
     (err, row) => {
       if (err) reject(err);
       else resolve(row);
